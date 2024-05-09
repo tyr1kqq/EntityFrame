3f294906b7f850c5f3cf13f775d034e7011630d9
tyrik <tyrik@DESKTOP-1GNPUGQ>
2024-05-09 10:39:30 +0200

    Добавьте файлы проекта.

---
+++ EntityFrame/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrame
+{
+    public class AppContext : DbContext
+    {
+        // Объекты такблицы users
+        public DbSet<User> Users { get; set; }
+        public AppContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-1GNPUGQ;Database=EF;Trusted_Connection=True;");
+        }
+    }
+}
